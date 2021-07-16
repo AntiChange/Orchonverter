fcf93ecb057b94cd9fad04091f32045c594f0ad0
Nancy Shen <46879202+nancyluyishen@users.noreply.github.com>
2021-07-16 16:57:21 -0400

    update melody separator server and output

--- melody_separator/src/app.py
+++ melody_separator/src/app.py
@@ -14,7 +14,7 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(port = 8000, host='0.0.0.0')
+    app.run(port = 8000, host='0.0.0.0',debug=True)
     # http_server = WSGIServer(('0.0.0.0', 8000), app)
     # http_server.serve_forever()
 #     # uvicorn.run(app, port=8000, host='0.0.0.0')
