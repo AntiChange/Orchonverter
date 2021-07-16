3ad8f29a49c805fd94b53fa3c5715b8184716976
nancyluyishen <nancyshen200088@gmail.com>
2021-07-15 20:52:26 -0400

    data preparation and loading model from SPICE

---
+++ SPICE/data_prep.py
@@ -0,0 +1,33 @@
+from pydub import AudioSegment
+from scipy.io import wavfile
+
+# Function that converts the user-created audio to the format that the model 
+# expects: bitrate 16kHz and only one channel (mono).
+
+EXPECTED_SAMPLE_RATE = 16000
+
+def convert_audio_for_model(user_file, output_file='converted_audio_file.wav'):
+  audio = AudioSegment.from_file(user_file)
+  audio = audio.set_frame_rate(EXPECTED_SAMPLE_RATE).set_channels(1)
+  audio.export(output_file, format="wav")
+  return output_file
+
+  # Converting to the expected format for the model
+# in all the input 4 input method before, the uploaded file name is at
+# the variable uploaded_file_name
+converted_audio_file = convert_audio_for_model("09-Jesus_fast_violin.wav")
+
+# Loading audio samples from the wav file:
+sample_rate, audio_samples = wavfile.read(converted_audio_file, 'rb')
+
+# Show some basic information about the audio.
+duration = len(audio_samples)/sample_rate
+print(f'Sample rate: {sample_rate} Hz')
+print(f'Total duration: {duration:.2f}s')
+print(f'Size of the input: {len(audio_samples)}')
+
+# # Let's listen to the wav file.
+# Audio(audio_samples, rate=sample_rate)
+
+MAX_ABS_INT16 = 32768.0
+audio_samples = audio_samples / float(MAX_ABS_INT16)
---
+++ SPICE/model.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+
+from data_prep import audio_samples
+
+# Loading the SPICE model is easy:
+model = hub.load("https://tfhub.dev/google/spice/2")
+
+# We now feed the audio to the SPICE tf.hub model to obtain pitch and uncertainty outputs as tensors.
+model_output = model.signatures["serving_default"](tf.constant(audio_samples, tf.float32))
+
+pitch_outputs = model_output["pitch"]
+uncertainty_outputs = model_output["uncertainty"]
+
+# 'Uncertainty' basically means the inverse of confidence.
+confidence_outputs = 1.0 - uncertainty_outputs
+
+# fig, ax = plt.subplots()
+# fig.set_size_inches(20, 10)
+# plt.plot(pitch_outputs, label='pitch')
+# plt.plot(confidence_outputs, label='confidence')
+# plt.legend(loc="lower right")
+# plt.show()
+
+confidence_outputs = list(confidence_outputs)
+pitch_outputs = [ float(x) for x in pitch_outputs]
+
+indices = range(len (pitch_outputs))
+confident_pitch_outputs = [ (i,p)  
+  for i, p, c in zip(indices, pitch_outputs, confidence_outputs) if  c >= 0.8  ]
+confident_pitch_outputs_x, confident_pitch_outputs_y = zip(*confident_pitch_outputs)
+ 
+# fig, ax = plt.subplots()
+# fig.set_size_inches(20, 10)
+# ax.set_ylim([0, 1])
+# plt.scatter(confident_pitch_outputs_x, confident_pitch_outputs_y, )
+# plt.scatter(confident_pitch_outputs_x, confident_pitch_outputs_y, c="r")
+
+# plt.show()
+
+def output2hz(pitch_output):
+  # Constants taken from https://tfhub.dev/google/spice/2
+  PT_OFFSET = 25.58
+  PT_SLOPE = 63.07
+  FMIN = 10.0;
+  BINS_PER_OCTAVE = 12.0;
+  cqt_bin = pitch_output * PT_SLOPE + PT_OFFSET;
+  return FMIN * 2.0 ** (1.0 * cqt_bin / BINS_PER_OCTAVE)
+    
+confident_pitch_values_hz = [ output2hz(p) for p in confident_pitch_outputs_y ]
+print(confident_pitch_values_hz)
+
